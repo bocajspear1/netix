356bf7198cf42ba91e58f60721c2ed7e5164df88
Jacob Hartman <junyunharold1@gmail.com>
2023-09-23 18:20:38 -0400

    Install fixes

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-apt-get update
-<<<<<<< HEAD
-apt-get install -y virtualbox-guest-additions open-vm-tools-desktop python3-pip python-venv mininet python3-tk wireshark tcpdump wget curl mousepad netcat-openbsd socat inetutils-traceroute add-apt-repository
-=======
-apt-get install -y virtualbox-guest-utils open-vm-tools-desktop python3-pip python3-venv mininet python3-tk wireshark tcpdump wget curl mousepad netcat-openbsd socat inetutils-traceroute
->>>>>>> c798fbd0a49115f4ba19ad77afc8ac7f27cc61bb
+COLOR_RED="\e[1;31m"
+COLOR_BLUE="\e[1;34m"
+COLOR_GREEN="\e[1;32m"
+COLOR_ORANGE="\e[1;33m"
+COLOR_RESET="\e[0m"
 
-echo "Install FRR..."
+apt-get update
+echo "${COLOR_BLUE}Installing packages...${COLOR_RESET}"
+apt-get install -y virtualbox-guest-additions open-vm-tools-desktop python3-pip python-venv mininet python3-tk wireshark tcpdump wget curl mousepad netcat-openbsd socat inetutils-traceroute add-apt-repository
+
+echo "${COLOR_BLUE}Installing FRR...${COLOR_RESET}"
 # add GPG key
 curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null
 
@@ -25,7 +28,7 @@ echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.o
 # update and install FRR
 apt update && apt install -y frr frr-pythontools
 
-echo "Install Faucet..."
+echo "${COLOR_BLUE}Installing Faucet...${COLOR_RESET}"
 apt-get install -y curl gnupg apt-transport-https lsb-release
 mkdir -p /etc/apt/keyrings/
 curl -1sLf https://packagecloud.io/faucetsdn/faucet/gpgkey | gpg --dearmor -o /etc/apt/keyrings/faucet.gpg
@@ -33,32 +36,34 @@ echo "deb [signed-by=/etc/apt/keyrings/faucet.gpg] https://packagecloud.io/fauce
 apt-get update
 apt-get install -y faucet python-faucet
 
-echo "Enabling forwarding..."
+echo "${COLOR_BLUE}Enabling forwarding...${COLOR_RESET}"
 echo "net.ipv4.ip_forward=1" | tee --append /etc/sysctl.conf
 echo "net.ipv6.conf.all.forwarding=1" | tee --append /etc/sysctl.conf
 sysctl -p
 
-echo "Installing custom scripts..."
+echo "${COLOR_BLUE}Installing custom scripts...${COLOR_RESET}"
 chmod +x ./scripts/*
 cp ./scripts/* /usr/local/bin/
 
-echo "Installing custom apps..."
+echo "${COLOR_BLUE}Installing custom apps...${COLOR_RESET}"
 cp -r ./apps/minieditng /opt/minieditng
 
-echo "Installing custom .desktop files..."
+echo "${COLOR_BLUE}Installing custom .desktop files...${COLOR_RESET}"
 cp ./desktop/* /usr/share/applications/
 
 # https://askubuntu.com/questions/944685/pasting-external-text-into-xterm-or-uxterm
-echo "Install xterm copy-paste fixes..."
+echo "${COLOR_BLUE}Install xterm copy-paste fixes...${COLOR_RESET}"
 for homedir in /home/*; do cp ./configs/Xresources "${homedir}/.Xresources"; done
 
 cp ./configs/Xresources /root/.Xresources
 
-echo "Install topologies"
+echo "${COLOR_BLUE}Install topologies${COLOR_RESET}"
 mkdir -p /opt/topologies
 cp ./examples/* /opt/topologies
 chmod 444 /opt/topologies/*
 
+
+echo "${COLOR_BLUE}Using Firefox DEB to save space from snaps...${COLOR_RESET}"
 snap remove --purge firefox
 apt remove --autoremove firefox
 
